f32406a148e1cc30432a4628a8c3cba3eff2b86c
Amir Braham <amirbrahamm@gmail.com>
2017-10-16 20:38:23 +0100

    Added DOTWEEN for smoother animations and working Play Button
    + More organized Folders and Files

---
+++ Bombs/Assets/Script/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+
+public class GameManager : MonoBehaviour {
+    [SerializeField]
+    GameObject[] UiItems;
+
+	// Use this for initialization
+	void Start () {
+         // GAME PAUSED AT FIRST 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartGame () {
+        GameObject.FindGameObjectWithTag("SpawnManager").GetComponent<BombSpawner>().enabled = true;
+        GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerScript>().enabled = true;
+
+        foreach(GameObject ui in UiItems) {
+            ui.transform.DOLocalMoveX(500f, 2f);
+        }
+    }
+    public void ChooseLevel () {
+        //ADD MORE LEVELS and Modifications 
+    }
+    public void SeeScoreBoard () {
+        //ADD SCOREBOARD
+    }
+    public void SeeSettings  () {
+        
+    }
+    public void ToggleSound () {
+        
+    }
+}
