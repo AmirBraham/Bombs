07741f69511391b873f0887a8ffaa89df2413947
Amir Braham <amirbrahamm@gmail.com>
2017-10-21 14:15:37 +0100

    smooth start animation

--- Bombs/Assets/Script/GameManager.cs
+++ Bombs/Assets/Script/GameManager.cs
@@ -5,16 +5,21 @@ using DG.Tweening;
 public class GameManager : MonoBehaviour {
     [SerializeField]
     GameObject[] UiItems;
-
+    [SerializeField]
+    GameObject CameraAnimator;
 	// Use this for initialization
 	void Start () {
-         // GAME PAUSED AT FIRST 
+        CameraAnimator.transform.DOMoveY(0, 3f).OnComplete(BringUI);
 	}
+
+    void BringUI () {
+        foreach (GameObject ui in UiItems)
+        {
+            ui.transform.DOLocalMoveX(0f, 2f);
+        }
+    }
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	// Update is called once per f
 
     public void StartGame () {
         GameObject.FindGameObjectWithTag("SpawnManager").GetComponent<BombSpawner>().enabled = true;
