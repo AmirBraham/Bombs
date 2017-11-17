48214fed652cbe60a669f1a34e5d061b37ef9a22
Amir Braham <amirbrahamm@gmail.com>
2017-11-17 21:51:04 +0100

    Sound Icon Toggle and added Soundtrack

--- Bombs/Assets/Script/GameManager.cs
+++ Bombs/Assets/Script/GameManager.cs
@@ -1,15 +1,21 @@
 ﻿using UnityEngine;
 using DG.Tweening;
-
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
     [SerializeField]
     GameObject[] UiItems;
     [SerializeField]
     GameObject CameraAnimator;
+    [SerializeField]
+    Sprite[] SoundIcons;
+    [SerializeField]
+    AudioSource Audio;
 
-	// Use this for initialization
+
+    // Use this for initialization
 	void Start () {
+        Audio.PlayDelayed(2f);
         CameraAnimator.transform.DOMoveY(0, 3f).OnComplete(BringUI);
 	}
 
@@ -19,9 +25,11 @@ public class GameManager : MonoBehaviour {
             ui.transform.DOLocalMoveX(0f, 2f);
         }
     }
-	
-	// Update is called once per f
 
+    // Update is called once per f
+    void Update()
+    {
+    }
     public void StartGame () {
         GameObject.FindGameObjectWithTag("SpawnManager").GetComponent<BombSpawner>().enabled = true;
         GameObject.FindGameObjectWithTag("SpawnManager").GetComponent<BombSpawner>().Dead = false;
@@ -40,8 +48,15 @@ public class GameManager : MonoBehaviour {
     public void SeeSettings  () {
         
     }
-    public void ToggleSound () {
-        
+    public void ToggleSound (Button button) {
+        Debug.Log(button.name);
+        if(button.GetComponent<Image>().sprite.name == "mute") {
+            button.GetComponent<Image>().sprite = SoundIcons[0];
+            Audio.mute = false;
+        } else {
+            button.GetComponent<Image>().sprite = SoundIcons[1];
+            Audio.mute = true;
+        }
     }
     public void RestartGame ( ) {
         foreach (GameObject ui in UiItems)
