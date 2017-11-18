6c644a1da562b169dfc9a4c1991ab5c3e2f34121
Amir Braham <amirbrahamm@gmail.com>
2017-11-18 22:26:53 +0100

    Changed Explosion styling ( for optimization)

---
+++ Bombs/Assets/Script/AutoDesktroy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDesktroy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnEnable()
+        {
+            Destroy(gameObject, 2.2f);
+        }
+}
