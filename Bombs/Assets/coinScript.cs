b730cf0273d1c255ce50bb12dd02f5eb5605b18a
Amir Braham <amirbrahamm@gmail.com>
2018-01-31 23:15:32 +0100

    added score animation and fixed plane rotation

--- Bombs/Assets/coinScript.cs
+++ Bombs/Assets/coinScript.cs
@@ -3,32 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 public class coinScript : MonoBehaviour {
-	public int prize;
-	float lifeTime = 3;
-	BombSpawner bombSpawner;
-	// Use this for initialization
-	void Start () {
+    public int prize;
+    float lifeTime = 3;
+    BombSpawner bombSpawner;
+    public GameObject ScoreAnimation;
+    // Use this for initialization
+    void Start () {
         transform.DOScale(new Vector3(1,1,1),2f);
-		bombSpawner = GameObject.Find ("Spawner").GetComponent<BombSpawner> ();
-		lifeTime = bombSpawner.spawnCoinTimer;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		transform.Rotate (Vector3.up * 2);
+        bombSpawner = GameObject.Find ("Spawner").GetComponent<BombSpawner> ();
+        lifeTime = bombSpawner.spawnCoinTimer;
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        transform.Rotate (Vector3.up * 2);
 
 
-		lifeTime -= Time.deltaTime;
-		if (lifeTime <= 0) {
-			Destroy (gameObject);
-		}
-	}
+        lifeTime -= Time.deltaTime;
+        if (lifeTime <= 0) {
+            Destroy (gameObject);
+        }
+    }
     void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player") {
             GameObject.FindGameObjectWithTag("GameManager").GetComponent<ScoreManager>().Increment(prize);
             transform.DOScale(new Vector3(0, 0, 0), 1f);
             gameObject.transform.GetChild(1).gameObject.SetActive(true);
+            GameObject scoreAnim = Instantiate(ScoreAnimation, new Vector3(240,0,0), Quaternion.identity);
+            scoreAnim.transform.parent = GameObject.FindGameObjectWithTag("canvas").transform;
             Destroy(gameObject,5f);
         }
 
